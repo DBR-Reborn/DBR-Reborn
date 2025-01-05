9f4313887a251e4f74a7c854c6b566c87c376863
DanMcCallum <daniel.mcc@gmail.com>
2025-01-04 19:55:35 -0700

    copying over repo

---
+++ mudlib/d/nocte/kuril/inn/h22.c
@@ -0,0 +1,16 @@
+// Hallway of the Wretched Zombie Inn
+
+#include <std.h>
+
+inherit ROOM;
+
+void create() {
+	::create();
+	set_property("light", 2);
+	set_short("Hallway of the Wretched Zombie Inn");
+	set_long(
+		"You are in a hallway of the Wretched Zombie Inn"
+	);
+	add_exit("/d/nocte/kuril/inn/h32", "north");
+	add_exit("/d/nocte/kuril/inn/h12","south");
+}
